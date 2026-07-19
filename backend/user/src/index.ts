5c77444fa307b2e9f36ab11b641f620a41480c95
Pawan Pawar <pawanpawar4369@gmail.com>
2026-07-19 22:23:53 +0530

    feat: add MongoDB connection configuration

--- backend/user/src/index.ts
+++ backend/user/src/index.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDB from "./config/connectDB.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+await connectDB();
 
 // Define a simple route
 app.get("/", (req, res) => {
