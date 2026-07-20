6aed3c2b735c6f77fbfcf4a2e9136df8c2af9f45
Pawan Pawar <pawanpawar4369@gmail.com>
2026-07-20 11:17:34 +0530

    feat: add Redis client configuration

--- backend/user/src/config/connectDB.ts
+++ backend/user/src/config/connectDB.ts
@@ -1,21 +1,21 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
 const connectDB = async () => {
-    const MONGO_URI = process.env.MONGO_URI;
-    
-    if(!MONGO_URI) {
-        throw new Error("Failed to resolve MONGO_URI from Environment Variables.");
-    }
+  const MONGO_URI = process.env.MONGO_URI;
 
-    try {
-        await mongoose.connect(MONGO_URI, {
-            dbName: "GridChat_DB"
-        });
-        console.log("MongoDB connected successfully!");
-    } catch (error) {
-        console.log("Error connecting to MongoDB...", error);
-        process.exit(1);
-    }
-}
+  if (!MONGO_URI) {
+    throw new Error("Failed to resolve MONGO_URI from Environment Variables.");
+  }
 
-export default connectDB
+  try {
+    await mongoose.connect(MONGO_URI, {
+      dbName: "GridChat_DB",
+    });
+    console.log("🍃 MongoDB connected successfully!");
+  } catch (error) {
+    console.log("Error connecting to MongoDB...", error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
--- backend/user/src/index.ts
+++ backend/user/src/index.ts
@@ -1,13 +1,29 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/connectDB.js";
+import { createClient } from "redis";
 
-dotenv.config();
+dotenv.config({ quiet: true });
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 await connectDB();
+
+const REDIS_URL = process.env.REDIS_URL;
+
+if (!REDIS_URL) {
+  throw new Error("Failed to resolve REDIS_URL from Environment Variables.");
+}
+
+const redisClient = createClient({
+  url: REDIS_URL,
+});
+
+redisClient
+  .connect()
+  .then(() => console.log("🔴 Redis connected successfully!"))
+  .catch((err) => console.error(err));
 
 // Define a simple route
 app.get("/", (req, res) => {
@@ -16,5 +32,7 @@ app.get("/", (req, res) => {
 
 // Start the server
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`🚀 Server running on port ${PORT}`);
 });
+
+export default redisClient;
